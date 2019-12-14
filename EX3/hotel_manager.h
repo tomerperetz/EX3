c49ede26df9618781d38071100533aac3413022f
tomerperetz <tomerperetz@gmail.com>
2019-12-14 17:50:45 +0200

    guest and rooms struct added

---
+++ EX3/hotel_manager.h
@@ -0,0 +1,37 @@
+#pragma once
+
+
+/*
+====================================================================================================================
+Description:
+Caculate student grade main functions
+====================================================================================================================
+*/
+
+// Includes --------------------------------------------------------------------------------------------------------
+#pragma once
+#include "hardCodedData.h"
+
+// Structs ---------------------------------------------------------------------------------------------------------
+
+typedef struct _guest_struct
+{
+	char *name[MAX_GUEST_NAME_LEN];
+	int ID;
+	int room_number;
+	int status;
+} guest_struct;
+
+typedef struct _hotel_room_struct
+{
+	char *name[MAX_GUEST_NAME_LEN];
+	int ID;
+	int price_pp;
+	int capacity;
+	int availablity;
+	int next_day_availablity;
+	int waiting_guest_counter;
+} hotel_room_struct;
+
+// Declerations ---------------------------------------------------------------------------------------------------
+
