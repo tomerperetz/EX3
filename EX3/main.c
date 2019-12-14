89e1eee29bca4aebb8b3e8c3258c3baa0b9c08e8
tomerperetz <tomerperetz@gmail.com>
2019-12-14 17:32:52 +0200

    Project initialize

---
+++ EX3/main.c
@@ -0,0 +1,30 @@
+/*
+====================================================================================================================
+Authors:
+	- Segev Elmalem, ID: 203149000
+	- Tomer Peretz, ID: 305002206
+Project: TestManager
+Input: students grades directory path
+Outputs: <user_path>/final_grades.txt
+Description:
+Calculate grades for each student and prints summary to output file.
+====================================================================================================================
+*/
+
+// Includes --------------------------------------------------------------------
+
+#include "hardCodedData.h"
+
+
+// Functions --------------------------------------------------------------------
+int main(int argc, char *argv[])
+{
+	// Checks whether the given arguments are valid  
+	if (ensureArgs(argc, EXPECTED_ARGC, argv) != TRUE) {
+		raiseError(1, __FILE__, __func__, __LINE__, ERROR_ID_1_ARGS);
+		return TRUE;
+	}
+
+	printf("done\n");
+	return FALSE;
+}
